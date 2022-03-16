4af3e01c26fe66de4b4c3b0bc084ae09cd7b242a
Nick <nick.portell12@gmail.com>
2022-03-15 22:18:22 -0400

    Added PokemonCollection and Mapping methods

--- Web.UI/PokeapiService/Repositories/PokemonRepository.cs
+++ Web.UI/PokeapiService/Repositories/PokemonRepository.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Web.UI.Dto.Request;
 using Web.UI.Dto.Response;
-using System.Net.Http.Json;
-using System.Net.Http;
+using Web.UI.Models.Pokemon;
 
 namespace Web.UI.PokeapiService.Repositories
 {
@@ -15,17 +17,36 @@ namespace Web.UI.PokeapiService.Repositories
         {
             // This should probably be initialized in Singleton scope?
             PokemonClient = new HttpClient();
-            PokemonClient.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+            PokemonClient.BaseAddress = new Uri("https://pokeapi.co/api/v2/pokemon");
         }
 
+        public async Task<GetPokemonCollectionResponseDto> GetPokemonCollection(GetPokemonCollectionRequestDto request)
+        {
+            try
+            {
+                string requestString = PokemonClient.BaseAddress + $"?limit={request.Limit}&offset={request.Offset}";
+                var response = await PokemonClient.GetFromJsonAsync<GetPokemonCollectionResponseDto>(requestString);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                var response = new GetPokemonCollectionResponseDto()
+                {
+                    Status = "500",
+                    Message = "Error: Request for Pokemon Collection was invalid."
+                };
+                return response;
+            }
+        }
+        
         public async Task<GetPokemonResponseDto> GetPokemonById(GetPokemonRequestDto request)
         {
             try
             {
-                var response = await PokemonClient.GetFromJsonAsync<GetPokemonResponseDto>(PokemonClient.BaseAddress + $"pokemon/{request.PokemonId}");
+                var response = await PokemonClient.GetFromJsonAsync<GetPokemonResponseDto>(PokemonClient.BaseAddress + $"/{request.PokemonId}");
                 return response;
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
                 var response = new GetPokemonResponseDto()
                 {
@@ -40,10 +61,10 @@ namespace Web.UI.PokeapiService.Repositories
         {
             try
             {
-                var response = await PokemonClient.GetFromJsonAsync<GetPokemonResponseDto>(PokemonClient.BaseAddress + $"pokemon/{request.PokemonName}");
+                var response = await PokemonClient.GetFromJsonAsync<GetPokemonResponseDto>(PokemonClient.BaseAddress + $"/{request.PokemonName}");
                 return response;
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
                 var response = new GetPokemonResponseDto()
                 {
@@ -52,6 +73,58 @@ namespace Web.UI.PokeapiService.Repositories
                 };
                 return response;
             }
+        }
+
+        public async Task<PokemonCollection> MapPokemonCollection(int limit, int offset)
+        {
+            var request = new GetPokemonCollectionRequestDto
+            {
+                Limit = limit,
+                Offset = offset
+            };
+
+            var response = await GetPokemonCollection(request);
+
+            PokemonCollection collection = new PokemonCollection
+            {
+                Count = response.Count,
+                Next = response.Next,
+                Previous = response.Previous,
+                Results = response.Results
+            };
+
+            return collection;
+        }
+        
+        public async Task<PokemonData> MapPokemonToPokemonData(PokemonData pokemon)
+        {
+            var request = new GetPokemonRequestDto
+            {
+                PokemonId = pokemon.Id,
+                PokemonName = pokemon.Name
+            };
+
+            var response = await GetPokemonByName(request);
+
+            pokemon.Abilities = response.Abilities;
+            pokemon.Base_Experience = response.Base_Experience;
+            pokemon.Forms = response.Forms;
+            pokemon.Game_Indices = response.Game_Indices;
+            pokemon.Height = response.Height;
+            pokemon.Held_Items = response.Held_Items;
+            pokemon.Id = response.Id;
+            pokemon.Is_Default = response.Is_Default;
+            pokemon.Location_Area_Encounters = response.Location_Area_Encounters;
+            pokemon.Moves = response.Moves;
+            pokemon.Order = response.Order;
+            pokemon.Past_Types = response.Past_Types;
+            pokemon.Species = response.Species;
+            pokemon.Sprites = response.Sprites;
+            pokemon.Stats = response.Stats;
+            pokemon.Types = response.Types;
+            pokemon.Weight = response.Weight;
+
+            return pokemon;
         }
     }
 }
