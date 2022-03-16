c6ca1c7aa16e24e14bceaf1c1518650207274de3
Nick <nick.portell12@gmail.com>
2022-03-15 22:15:43 -0400

    Added new Dtos

---
+++ Web.UI/Dto/Request/GetPokemonBaseSpritesRequestDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Web.UI.Dto.Request
+{
+    public class GetPokemonBaseSpritesRequestDto
+    {
+        public string Back_Default { get; set; }
+
+        public string Back_Female { get; set; }
+
+        public string Back_Shiny { get; set; }
+
+        public string Back_Shiny_Female { get; set; }
+
+        public string Front_Default { get; set; }
+
+        public string Front_Female { get; set; }
+
+        public string Front_Shiny { get; set; }
+
+        public string Front_Shiny_Female { get; set; }
+    }
+}
---
+++ Web.UI/Dto/Request/GetPokemonCollectionRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.UI.Dto.Request
+{
+    public class GetPokemonCollectionRequestDto
+    {
+        public int Limit { get; set; }
+
+        public int Offset { get; set; }
+    }
+}
