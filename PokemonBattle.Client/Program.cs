fed6290d854bc7b1d2071d4771b60ed574032b68
Nick Portell <nportell@uwm.com>
2022-05-02 13:50:00 -0400

    Added local storage for clientside

--- PokemonBattle.Client/Program.cs
+++ PokemonBattle.Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace PokemonBattle.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
         }
