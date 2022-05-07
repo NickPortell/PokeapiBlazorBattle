6a342e78f4a355d2b92f8ad55c518f447f5f190f
Nick <nick.portell12@gmail.com>
2022-05-06 21:13:31 -0400

    Added scoped services

--- PokemonBattle.Client/Program.cs
+++ PokemonBattle.Client/Program.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PokemonBattle.Client.Controllers;
+using PokemonBattle.Client.Orchestrators;
+using PokemonBattle.Client.StateManagement;
+using PokemonBattle.Infrastructure.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -20,7 +24,11 @@ namespace PokemonBattle.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBlazoredLocalStorage();
-            builder.Services.AddScoped<StateContainer>();
+            builder.Services.AddScoped<StateManager>();
+            builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+            builder.Services.AddScoped<HttpClient>();
+            builder.Services.AddScoped<IPokemonOrchestrator, PokemonOrchestrator>();
+            builder.Services.AddScoped<PokemonController>();
             await builder.Build().RunAsync();
         }
     }
---
+++ PokemonBattle.Client/StateManagement/StateManager.cs
@@ -0,0 +1,13 @@
+﻿using PokemonBattle.Models.V1.Pokemon;
+using System.Collections.Generic;
+
+namespace PokemonBattle.Client.StateManagement
+{
+    public class StateManager
+    {
+        public PokemonCollection Pokedex { get; set; }
+
+        public List<PokemonData> PokemonTeam { get; set; }
+
+    }
+}
