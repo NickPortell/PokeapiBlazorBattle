05c0de5d9a0eed6177e0d3205abc118b138900b5
Nick <nick.portell12@gmail.com>
2022-05-15 01:55:33 -0400

    Created partial class, removed unused pages, and switched to all blazor logic

---
+++ PokemonBattle.Client/Shared/NavMenu.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonBattle.Client.Shared
+{
+    public partial class NavMenu : ComponentBase
+    {
+        private string NavBarClass = "";
+
+        private void ToggleNavMenu(MouseEventArgs args)
+        {
+            List<string> classList = NavBarClass.Split(' ').ToList();
+
+            if (classList.Contains("collapse") == false)
+            {
+                classList.Add("collapse");
+            }
+            else
+            {
+                classList.Remove("collapse");
+            }
+
+            NavBarClass = string.Join(' ', classList).Trim();
+        }
+    }
+}
